c453030a166fbb01114571a5827c85f47b3e99cc
lilcuz03 <taffychiri@gmail.com>
2026-05-31 22:22:09 +0200

    Unify navbar/footer via layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/NavBar";
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +31,11 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        <div className="pt-18 flex flex-col flex-1">{children}</div>
+        <Footer />
+      </body>
     </html>
   );
 }
