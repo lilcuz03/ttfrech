5ac7c0359beec9c2b020dbc1c73abd267e3c1cba
lilcuz03 <taffychiri@gmail.com>
2026-06-01 07:50:04 +0200

    Add robots route

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,12 @@
 
-import type { MetadataRoute } from "next";
-
-export default function robots(): MetadataRoute.Robots {
+export default function robots() {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
     sitemap: "https://www.ttfrech.co.za/sitemap.xml",
   };
 }
-
